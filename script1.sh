6311ef529aff414cf5f36042c79b66dfeb2146f4
Akash-M-Ubale <akashubale70@gmail.com>
2024-08-26 12:32:20 +0530

    Janmashtmi commit

--- script1.sh
+++ script1.sh
@@ -2,4 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "new practical is been processed again"
 echo "abcdeeegjlgjdljdojdljglgjgolj;o"
-echo "nisha gajbhiye"
+echo ""Its a wonderful morning happy jabmasahtmi
